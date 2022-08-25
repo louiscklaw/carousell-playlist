223697aba3496094785882930bcd0996c0bc61e3
Louis Law <louiscklaw@users.noreply.github.com>
2022-08-25 19:42:14 +0800

    update assert,

--- carousell-scripts/search-scan/cypress/e2e/carousell_helloworld.cy.js
+++ carousell-scripts/search-scan/cypress/e2e/carousell_helloworld.cy.js
@@ -1,6 +1,6 @@
 // 3D%20代客打印
 
-describe('3D 代客打印 - my post should appears in the first 8 cards', () => {
+describe('3D 代客打印 - my name should appears in the first 8 cards', () => {
   it('Visits https://www.carousell.com.hk', () => {
     cy.viewport('macbook-16');
 
@@ -11,13 +11,20 @@ describe('3D 代客打印 - my post should appears in the first 8 cards', () => 
     cy.xpath('(.//button[@data-testid="navbar-search-input-location-desktop-btn-search"])[1]').click();
     cy.wait(5000);
 
+    let my_name_found = false;
+
     Array(8)
       .fill(0)
       .map((_, idx) => {
         let el_idx = idx + 1;
-        cy.xpath(`(.//div[(@data-testid!="listing-card") and starts-with(@data-testid,"listing-card")]/div/a)[${el_idx}]`).then($ele => {
-          cy.debug($ele.text());
-        });
+        // cy.xpath(`(.//div[(@data-testid!="listing-card") and starts-with(@data-testid,"listing-card")]/div/a)[${el_idx}]`).then($ele => {
+        //   cy.debug($ele.text());
+        // });
+        // cy.xpath(`(.//*[@data-testid="listing-card-text-seller-name"])[${el_idx}]`).then($ele => {
+        //   cy.debug($ele.text());
+        // });
+
+        cy.xpath(`(.//*[@data-testid="listing-card-text-seller-name"])`).contains('louiscklaw');
       });
 
     cy.screenshot();
