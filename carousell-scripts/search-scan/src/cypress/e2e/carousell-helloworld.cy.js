c030de3f2c5c27db039882d751b6d3abaab63ad3
Louis Law <louiscklaw@users.noreply.github.com>
2022-08-27 01:32:27 +0800

    update and test,

--- carousell-scripts/search-scan/src/cypress/e2e/carousell-helloworld.cy.js
+++ carousell-scripts/search-scan/src/cypress/e2e/carousell-helloworld.cy.js
@@ -1,6 +1,38 @@
 describe('carousell', () => {
   it('helloworld', () => {
     cy.visit('https://www.carousell.com.hk');
+    cy.wait(1000 * 5);
+
+    cy.log('helloworld');
+    cy.screenshot();
+  });
+});
+
+describe('user search by 3d printing keywords - my name should appears in the first 8 cards', () => {
+  before(() => {
+    // runs once before all tests in the block
+  });
+
+  beforeEach(() => {
+    // runs before each test in the block
+    cy.clearCookies();
+    cy.clearLocalStorage();
+  });
+
+  afterEach(() => {
+    // runs after each test in the block
+    cy.clearCookies();
+    cy.clearLocalStorage();
+  });
+
+  after(() => {
+    // runs once after all tests in the block
+  });
+
+  it('helloworld', () => {
+    cy.visit('https://www.carousell.com.hk');
+    cy.wait(1000 * 5);
+
     cy.log('helloworld');
     cy.screenshot();
   });
