68ac9ddff7b253ac38bc8c5c1533adb5ea3fe040
louiscklaw <louiscklaw@gmail.com>
2022-09-08 17:54:24 +0800

    update and test,

--- carousell-scripts/search-scan/cypress/e2e/carousell.cy.js
+++ carousell-scripts/search-scan/cypress/e2e/carousell.cy.js
@@ -1,12 +1,12 @@
-import { onlyOn, skipOn } from '@cypress/skip-test';
+import { onlyOn, skipOn } from "@cypress/skip-test";
 
-Cypress.on('uncaught:exception', (err, runnable) => {
+Cypress.on("uncaught:exception", (err, runnable) => {
   // returning false here prevents Cypress from
   // failing the test
   return false;
 });
 
-describe('test xpath', { retries: { runMode: 3, openMode: 1 } }, () => {
+describe("test xpath", { retries: { runMode: 3, openMode: 1 } }, () => {
   beforeEach(() => {
     cy.clearLocalStorage();
     cy.clearCookies();
@@ -15,12 +15,12 @@ describe('test xpath', { retries: { runMode: 3, openMode: 1 } }, () => {
 
   // afterEach(() => {});
 
-  it(`try search by thingiverse`, () => {
-    cy.intercept('https://sentry.io/*', {});
-    cy.visit('https://www.carousell.com.hk');
+  it(`try search by carousell`, () => {
+    cy.intercept("https://sentry.io/*", {});
+    cy.visit("https://www.carousell.com.hk");
 
     cy.viewport(1920, 1080 * 10);
 
-    cy.screenshot({ capture: 'viewport', overwrite: true });
+    cy.screenshot({ capture: "viewport", overwrite: true });
   });
 });
