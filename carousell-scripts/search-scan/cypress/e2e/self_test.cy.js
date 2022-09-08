1419a1280d3981c0642acbaf6d3298c8a1ef2684
louiscklaw <louiscklaw@gmail.com>
2022-09-08 17:45:04 +0800

    update and test,

--- carousell-scripts/search-scan/cypress/e2e/self_test.cy.js
+++
@@ -1,7 +0,0 @@
-describe('My First Test', () => {
-  it('Visits https://louiscklaw.github.io/routine_check/', () => {
-    cy.visit('https://louiscklaw.github.io/routine_check/');
-
-    cy.screenshot();
-  });
-});
