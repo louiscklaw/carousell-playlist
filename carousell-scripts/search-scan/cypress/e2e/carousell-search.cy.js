fe02f3c7d75e19a350787fb5e42f8d462298387f
louiscklaw <louiscklaw@gmail.com>
2022-09-08 18:25:35 +0800

    update and test,

--- carousell-scripts/search-scan/cypress/e2e/carousell-search.cy.js
+++ carousell-scripts/search-scan/cypress/e2e/carousell-search.cy.js
@@ -1,5 +1,11 @@
 // 3D%20代客打印
 
+Cypress.on("uncaught:exception", (err, runnable) => {
+  // returning false here prevents Cypress from
+  // failing the test
+  return false;
+});
+
 describe("3D 代客打印 - my post should appears in the first 8 cards", () => {
   beforeEach(() => {
     cy.clearLocalStorage();
@@ -12,6 +18,9 @@ describe("3D 代客打印 - my post should appears in the first 8 cards", () => 
 
     cy.visit("https://www.carousell.com.hk");
     cy.xpath('(.//input[starts-with(@placeholder,"Search")])[1]').type("3D 代客打印");
+
+    cy.xpath('(.//button[@data-testid="navbar-search-input-location-desktop-btn-search"])[1]').click();
+    cy.wait(5000);
 
     cy.screenshot({ capture: "viewport", overwrite: true });
   });
