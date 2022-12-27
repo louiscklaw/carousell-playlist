bb3a10901a7e1d0223f7bf35620b15ec53ecda27
louiscklaw <louiscklaw@users.noreply.github.com>
2022-12-28 01:44:00 +0800

    update test screenshot,

---
+++ carousell-scripts/search-scan-screenshot/cypress/e2e/carousell-search-javascript.cy.js
@@ -0,0 +1,54 @@
+// 3D%20代客打印
+
+Cypress.on("uncaught:exception", (err, runnable) => {
+  // returning false here prevents Cypress from
+  // failing the test
+  return false;
+});
+
+describe("javascript - my post should appears in the first 8 cards", () => {
+  let louis_found = false;
+
+  beforeEach(() => {
+    cy.clearLocalStorage();
+    cy.clearCookies();
+    cy.wait(1000);
+
+    cy.intercept("https://sentry.io/*", {});
+  });
+
+  afterEach(() => {
+    expect(louis_found).to.be.true;
+  });
+
+  it("Visits https://www.carousell.com.hk", () => {
+    cy.viewport(1920, 1080 * 1);
+
+    // 3D 代客打印
+    cy.visit("https://www.carousell.com.hk/search/javascript");
+    cy.wait(10 * 1000);
+
+    // cy.xpath('(.//input[starts-with(@placeholder,"Search")])[1]').type(
+    //   "3D 代客打印"
+    // );
+
+    // cy.xpath('(.//button[@role="submitButton"])[1]').click();
+    // cy.wait(10 * 1000);
+
+    Array(8)
+      .fill(0)
+      .map((_, idx) => {
+        let el_idx = idx + 1;
+        cy.xpath(
+          `(.//div[(@data-testid!="listing-card") and starts-with(@data-testid,"listing-card")]/div/a)[${el_idx}]`
+        ).then(($ele) => {
+          cy.debug($ele.text());
+          if ($ele.text().search(/louiscklaw/)) {
+            louis_found = true;
+          }
+        });
+      });
+
+    cy.screenshot({ capture: "viewport", overwrite: true });
+  });
+});
