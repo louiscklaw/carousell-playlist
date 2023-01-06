ef293a6c64fa2ecf28b492fce79d86dc4e3db84a
louiscklaw <louiscklaw@users.noreply.github.com>
2023-01-06 22:27:41 +0800

    update path,

--- carousell-scripts/share.iamon99.com-upload-check/publish.js
+++ carousell-scripts/share.iamon99.com-upload-check/publish.js
@@ -2,11 +2,13 @@ var ghpages = require("gh-pages");
 var fs = require("fs");
 var path = require("path");
 
-report_name = path.basename(__dirname);
+report_name = path.basename(__dirname).replace(/\./g,'-');
 console.log({config: {report_name}})
 
 fs.renameSync("cypress/reports/html/index.html", `cypress/reports/html/${report_name}.html`);
 
-ghpages.publish("cypress/reports/html", { add: true }, () => {
-  console.log("publish done");
-});
+if (process.env.CI){
+  ghpages.publish("cypress/reports/html", { add: true }, () => {
+    console.log("publish done");
+  });
+}
