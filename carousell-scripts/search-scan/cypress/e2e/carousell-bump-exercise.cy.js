8eabac8df01d2033fdff1dfe6a127e44423e9547
louiscklaw <louiscklaw@users.noreply.github.com>
2023-01-06 19:15:06 +0800

    update,

--- carousell-scripts/search-scan/cypress/e2e/carousell-bump-exercise.cy.js
+++ carousell-scripts/search-scan/cypress/e2e/carousell-bump-exercise.cy.js
@@ -2,24 +2,22 @@ Cypress.on("uncaught:exception", (err, runnable) => {
   // returning false here prevents Cypress from
   // failing the test
   return false;
-
   // cy.debug(err);
 });
-
-Cypress.on('fail', (err, runnable) => {
-  return false
-})
 
 var ENV_KEYWORD_LIST = Cypress.env("ENV_KEYWORD_LIST");
 var ENV_USER_LIST = Cypress.env("ENV_USER_LIST");
 
 describe(`checking ${'active_keyword'}->${'active_user'}`, () => {
   it("carousell-bump-exercise.cy.js", () => {
+    cy.on('uncaught:exception', (err, runnable) => {
+        return false
+    })
     var USER_LIST = ENV_USER_LIST.split("++++");
     var KEYWORD_LIST = ENV_KEYWORD_LIST.split("++++");
     
     cy.readFile('ad_list.json',{encoding: 'utf-8'}).then(str => {
-      str.forEach( s => cy.intercept(`https://*.${s}/*`, {}))
+        str.forEach( s => cy.intercept(`https://*.${s}/*`, {}))
     })
 
     cy.intercept("https://*.googlesyndication.com/*", {});
@@ -38,21 +36,26 @@ describe(`checking ${'active_keyword'}->${'active_user'}`, () => {
         cy.clearLocalStorage();
         cy.clearCookies();
         
-        cy.visit(`https://www.carousell.com.hk/search/${kw}`);
-        cy.get('[data-testid="listing-card-text-seller-name"]')
-          .filter(`:contains("${active_user}")`)
-          .first()
-          .parent()
-          .parent()
-          .parent()
-          .click();
+        // cy.visit(`https://www.example.com/`);
+        // cy.visit(`http://192.168.10.180:5500/carousell-scripts/search-scan/site/index.html`);
 
-        cy.wait(3 * 1000);
+        cy.visit(`https://www.carousell.com.hk/search/${kw}`);
+        if (Cypress.$('[data-testid="listing-card-text-seller-name"]').length > 0) {
+          cy.get('[data-testid="listing-card-text-seller-name"]')
+            .find(`:contains("${active_user}")`)
+            .first()
+            .parent()
+            .parent()
+            .parent()
+            .click();
+          cy.wait(3 * 1000);
+        }else{
+          cy.log(`cannot find the user -> ${active_user}`)
+          cy.wait(1000)
+        }
 
       })
     })
-    
-
 
     cy.log({
       ENV_KEYWORD_LIST,
