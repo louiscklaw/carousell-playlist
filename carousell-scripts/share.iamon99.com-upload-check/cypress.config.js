b6012fcbae2d58117db17697e7ae1b30b74f5280
louiscklaw <louiscklaw@users.noreply.github.com>
2023-01-06 22:05:43 +0800

    update,

---
+++ carousell-scripts/share.iamon99.com-upload-check/cypress.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  video: true,
+  screenshotsFolder: 'images',
+  e2e: {
+    setupNodeEvents(on, config) {
+      require("cypress-mochawesome-reporter/plugin")(on);
+
+      // return require("./cypress/plugins/index.js")(on, config);
+    },
+  },
+});
---
+++ carousell-scripts/share.iamon99.com-upload-check/cypress/e2e/https-louiscklaw-github-io.cy.js
@@ -0,0 +1,35 @@
+import { onlyOn, skipOn } from "@cypress/skip-test";
+
+Cypress.on("uncaught:exception", (err, runnable) => {
+  // returning false here prevents Cypress from
+  // failing the test
+  return false;
+});
+
+describe("https://louiscklaw.github.io self-check", { retries: { runMode: 3, openMode: 1 } }, () => {
+
+  beforeEach(() => {
+    cy.clearLocalStorage();
+    cy.clearCookies();
+
+    cy.intercept("https://sentry.io/*", {});
+
+    cy.wait(1000);
+  });
+
+  // afterEach(() => {});
+
+  it(`https://louiscklaw.github.io self check`, () => {
+    cy.visit("https://louiscklaw.github.io/carousell");
+
+    cy.viewport(1920, 1080);
+
+    cy.screenshot({ capture: "viewport", overwrite: true });
+
+    cy.xpath(`/html/body/main/div/div`).then(($ele) => {
+      cy.debug($ele.text());
+      var result = $ele.text().search(/Hi, I am louis/);
+      expect(result > -1).to.be.true;
+    });
+  });
+});
