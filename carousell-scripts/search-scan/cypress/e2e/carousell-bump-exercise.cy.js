c9dd6e9c8ed8b0316e6238a61beb1e516c19033b
louiscklaw <louiscklaw@users.noreply.github.com>
2023-01-06 19:52:14 +0800

    update,

--- carousell-scripts/search-scan/cypress/e2e/carousell-bump-exercise.cy.js
+++ carousell-scripts/search-scan/cypress/e2e/carousell-bump-exercise.cy.js
@@ -8,11 +8,18 @@ Cypress.on("uncaught:exception", (err, runnable) => {
 var ENV_KEYWORD_LIST = Cypress.env("ENV_KEYWORD_LIST");
 var ENV_USER_LIST = Cypress.env("ENV_USER_LIST");
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
 describe(`carousell-bump-exercise.cy.js`, () => {
   it("carousell-bump-exercise.cy.js", () => {
     cy.on('uncaught:exception', (err, runnable) => {
         return false
     })
+
+    var time_to_click = getRandomInt(10)
+    
     var USER_LIST = ENV_USER_LIST.split("++++");
     var KEYWORD_LIST = ENV_KEYWORD_LIST.split("++++");
 
@@ -20,11 +27,13 @@ describe(`carousell-bump-exercise.cy.js`, () => {
         .map(value => ({ value, sort: Math.random() }))
         .sort((a, b) => a.sort - b.sort)
         .map(({ value }) => value)
-   
+
     let shuffled_USER_LIST = USER_LIST
       .map(value => ({ value, sort: Math.random() }))
       .sort((a, b) => a.sort - b.sort)
       .map(({ value }) => value)
+
+    cy.debug({config:{ time_to_click } })
 
     cy.readFile('ad_list.json',{encoding: 'utf-8'}).then(str => {
         str.forEach( s => cy.intercept(`https://*.${s}/*`, {}))
@@ -40,32 +49,34 @@ describe(`carousell-bump-exercise.cy.js`, () => {
     cy.intercept("https://*.admeme.net/*", {});
     cy.intercept("https://*.rlcdn.com/*", {});
 
-    shuffled_KEYWORD_LIST.forEach(kw => {
-      shuffled_USER_LIST.forEach(active_user => {
-        cy.log({kw, active_user})
-        cy.clearLocalStorage();
-        cy.clearCookies();
-        
-        // cy.visit(`https://www.example.com/`);
-        // cy.visit(`http://192.168.10.180:5500/carousell-scripts/search-scan/site/index.html`);
 
-        cy.visit(`https://www.carousell.com.hk/search/${kw}`);
-        if (Cypress.$('[data-testid="listing-card-text-seller-name"]').length > 0) {
-          cy.get('[data-testid="listing-card-text-seller-name"]')
-            .find(`:contains("${active_user}")`)
-            .first()
-            .parent()
-            .parent()
-            .parent()
-            .click();
-          cy.wait(3 * 1000);
-        }else{
-          cy.log(`cannot find the user -> ${active_user}`)
-          cy.wait(1000)
-        }
+    for(var ii=0; ii< time_to_click; ii++){
+      shuffled_KEYWORD_LIST.forEach(kw => {
+        shuffled_USER_LIST.forEach(active_user => {
+          cy.log({kw, active_user})
+          cy.clearLocalStorage();
+          cy.clearCookies();
+          
+          // cy.visit(`https://www.example.com/`);
+          cy.visit(`http://192.168.10.180:5500/carousell-scripts/search-scan/site/index.html`);
 
+          // cy.visit(`https://www.carousell.com.hk/search/${kw}`);
+          if (Cypress.$('[data-testid="listing-card-text-seller-name"]').length > 0) {
+            cy.get('[data-testid="listing-card-text-seller-name"]')
+              .find(`:contains("${active_user}")`)
+              .first()
+              .parent()
+              .parent()
+              .parent()
+              .click();
+            cy.wait(3 * 1000);
+          }else{
+            cy.log(`cannot find the user -> ${active_user}`)
+            cy.wait(1000)
+          }
+        })
       })
-    })
+    }
 
     cy.log({
       ENV_KEYWORD_LIST,
