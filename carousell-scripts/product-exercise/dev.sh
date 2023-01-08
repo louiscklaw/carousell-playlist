b81086b43b27a3b02f9a78755d380c24090bf446
louiscklaw <louiscklaw@users.noreply.github.com>
2023-01-08 14:26:43 +0800

    update,

--- carousell-scripts/product-exercise/dev.sh
+++ carousell-scripts/product-exercise/dev.sh
@@ -7,6 +7,14 @@ export ENV_USER_LIST=louiscklaw
 export ENV_MIN_CLICK=1
 export ENV_MAX_CLICK=3
 
+
+docker container prune -f
+docker system prune -f
+docker image prune -f
+docker volume prune -f
+docker network prune -f
+
+
 docker compose \
   -f docker-compose.dev.yml \
   up -d --build
